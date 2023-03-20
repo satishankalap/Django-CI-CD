b13fb07a29d27f95a2e1568c15baf31e4a865bf8
satish ankalap <satishankalap96@gmail.com>
2023-03-20 13:03:15 +0000

    deleted run.sh from appspec

--- scripts/code.sh
+++ scripts/code.sh
@@ -4,3 +4,7 @@ sudo apt-get update
 sudo apt-get install -y python3-pip python3-dev libpq-dev
 pip3 install --upgrade pip
 pip3 install Django==3.2.9
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py runserver 0.0.0.0:8000
+
